b2b7370a9767fcb661b4870337735d8d5e0d1263
Rishat <risatnurullin53@gmail.com>
2022-08-17 09:09:44 +0300

    task1

---
+++ tas1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите размер массива m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размер массива n ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] arr = new double[m, n];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1); j++)
+    {
+        arr[i, j] = new Random().NextDouble();
+        Console.Write(arr[i, j] + " ");
+    }
+    Console.WriteLine();
+}
